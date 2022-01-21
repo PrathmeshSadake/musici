a30a061fc43e7320418f9da3b032aca53c06eae6
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-01-22 02:01:39 +0530

    Adding assets and required cdn. Installed Tailwind css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./assets/css/tailwind.css";
+import "./assets/css/main.css";
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).use(store).use(router).mount("#app");
